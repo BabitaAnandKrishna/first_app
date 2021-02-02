ca858cd0cd9c60e1499ac804df43801e1e73fbb5
Babita <babita.softech@gmail.com>
2021-02-02 16:05:10 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/trynew/first_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package trynew.first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+/// Flutter code sample for Scaffold.of
+
+// Typical usage of the [Scaffold.of] function is to call it from within the
+// `build` method of a child of a [Scaffold].
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Code Sample for Scaffold.of.',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        body: MyScaffoldBody(),
+        appBar: AppBar(title: Text('Scaffold.of Example')),
+      ),
+      color: Colors.white,
+    );
+  }
+}
+
+class MyScaffoldBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: Text('SHOW A SNACKBAR'),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Hello!'),
+                duration:  Duration(minutes: 5),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
