23af20346a6363cbd6459f10f74abedcf356e600
Babita <babita.softech@gmail.com>
2021-02-13 22:46:29 +0000

    ListView Builder, List Generate, Card & Asserts #30DaysOfFlutter

--- lib/Pages/Home_page.dart
+++ lib/Pages/Home_page.dart
@@ -1,20 +1,29 @@
+import 'package:first_app/model/catelog.dart';
 import 'package:first_app/widgets/drawer.dart';
+import 'package:first_app/widgets/item_widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
-  final String name = "devlopment";
+  final String name = "Devlopment";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return  Scaffold(
       appBar: AppBar(
         title: Text("Catelog App"),
       ),
-      body:Center(
-       child: Container(
-        child: Text("Welcome to $days days of Flutter $name"),
-        // child: Text("Welcome to $days days of Flutter + name"),
-        ),
+      body:Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          // itemCount: CatalogModel.items.length,
+            itemCount: dummyList.length,
+            itemBuilder: (context,index){
+            return ItemWidgets(
+              // item: CatalogModel.items[index],
+              item: dummyList[index],
+            );
+            }),
       ),
       drawer: MyDrawer(
 
